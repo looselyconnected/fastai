18d46b9332394a9cab9de47eb4b4c328b5305976
Ying Xie <yingxie3@gmail.com>
2024-04-22 22:02:40 -0700

    Add train.py for stock_llm

--- stock_llm/model.py
+++ stock_llm/model.py
@@ -107,11 +107,11 @@ class Block(nn.Module):
 
 @dataclass
 class GPTConfig:
-    block_size: int = 1024
-    vocab_size: int = 50304 # GPT-2 vocab_size of 50257, padded up to nearest multiple of 64 for efficiency
-    n_layer: int = 12
-    n_head: int = 12
-    n_embd: int = 768
+    block_size: int = 512
+    vocab_size: int = 4096
+    n_layer: int = 4
+    n_head: int = 4
+    n_embd: int = 128
     dropout: float = 0.0
     bias: bool = True # True: bias in Linears and LayerNorms, like GPT-2. False: a bit better and faster
 
